bec52cf6e7f3f168f38c038f62c01d040f1cb2a7
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-10 23:36:38 -0500

    04 - types of data

---
+++ 01_intro/04_types_of_data.py
@@ -0,0 +1,11 @@
+my_string = "This is a string"
+print(type(my_string)) # <class 'str'>
+
+my_integer = 1
+print(type(my_integer)) # <class 'int'>
+
+my_float = 1.0
+print(type(my_float)) # <class 'float'>
+
+my_boolean = True
+print(type(my_boolean)) # <class 'bool'>
