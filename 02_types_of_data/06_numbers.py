025c4ee1ed6e30ca52e031ccf14e4851c18b2658
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-18 19:10:19 -0500

    06 - numbers _ complex

--- 02_types_of_data/06_numbers.py
+++ 02_types_of_data/06_numbers.py
@@ -1,9 +1,11 @@
 my_int = 3
 my_float = 3.14
+complex_number = 3 + 2j
 
 
 print(type(my_int))  # <class 'int'>
 print(type(my_float))  # <class 'float'>
+print(type(complex_number))  # <class 'complex'>
 
 
 my_int += 2    # my_int = my_int + 2
