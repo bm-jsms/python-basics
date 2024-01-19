5def1a1d579cfe7ebe4c4c70e4d92eb14e645f63
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-18 19:07:59 -0500

    06 - numbers

---
+++ 02_types_of_data/06_numbers.py
@@ -0,0 +1,19 @@
+my_int = 3
+my_float = 3.14
+
+
+print(type(my_int))  # <class 'int'>
+print(type(my_float))  # <class 'float'>
+
+
+my_int += 2    # my_int = my_int + 2
+print(my_int)  # 5
+
+
+print(2 + 3)    # 5
+print(2 - 3)    # -1
+print(2 * 3)    # 6
+print(2 / 3)    # 0.6666666666666666
+print(2 // 3)   # 0
+print(2 % 3)    # 2
+print(2 ** 3)   # 8
