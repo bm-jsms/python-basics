0e7c6ec8e6c3fa8fbe8f99a6a96c1d2d0726a737
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-18 19:16:17 -0500

    07 - math module

---
+++ 02_types_of_data/07_math.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.pi)  # 3.141592653589793
+
+print(math.floor(1.99))  # 1
+print(math.ceil(1.01))  # 2
+
+print(math.sqrt(9))  # 3.0
+
+print(math.pow(2, 3))  # 8.0
+
+print(math.isnan(2))  # False
