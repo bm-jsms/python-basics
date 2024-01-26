51f7693e9fbb410d69411ab0a35d8c06268da386
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-01-25 22:27:31 -0500

    02 - if elif else

---
+++ 03_flow_control/02_if_elif_else.py
@@ -0,0 +1,20 @@
+car = 'Tesla'
+
+if car == 'Tesla':
+    print("I love Tesla!")
+else:
+    print("I don't like Tesla.")
+
+
+print("\n")
+print("*" * 30)
+
+
+age = 18
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
